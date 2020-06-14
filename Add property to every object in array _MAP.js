4a3248a5d2ba3d8aceedac9a6674eaf4bd0026f9
cyxoi1 <cyxoi1@yandex.ru>
2020-06-14 18:56:58 +0100

    Merge remote-tracking branch 'origin/master'

---
+++ Add property to every object in array _MAP.js
@@ -0,0 +1,4 @@
+//https://www.codewars.com/kata/54e8c3e89e2ae6f4900005a1
+
+
+questions.map(el => el.usersAnswer = null)
---
+++ Coding Meetup #5 - Higher-Order Functions Series - Prepare the count of languages.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/5828713ed04efde70e000346/train/javascript
+
+
+function countLanguages(list) {
+    let obj ={};
+    for(let i = 0; i < list.length; i++){
+        if(obj[list[i].language]) obj[list[i].language]++;
+        else obj[list[i].language] = 1;
+    }
+    return obj;
+}
